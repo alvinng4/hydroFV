01c1fe2946cae781155dc7e6649f2ec9243ed5ec
alvinng4 <alvinng324@gmail.com>
2025-02-15 22:22:01 +0800

    Change godunov calculation so that its correct for spherical, but not sure if its right

--- FiniteVolume1D/godunov_first_order.py
+++ FiniteVolume1D/godunov_first_order.py
@@ -34,9 +34,9 @@ def solving_step(system: System, dt: float, solver: str) -> None:
     system.momentum[:-1] -= flux_momentum * system.surface_area[:-1] * dt
     system.energy[:-1] -= flux_energy * system.surface_area[:-1] * dt
 
-    system.mass[1:] += flux_mass * system.surface_area[:-1] * dt
-    system.momentum[1:] += flux_momentum * system.surface_area[:-1] * dt
-    system.energy[1:] += flux_energy * system.surface_area[:-1] * dt
+    system.mass[1:] += flux_mass * system.surface_area[1:] * dt
+    system.momentum[1:] += flux_momentum * system.surface_area[1:] * dt
+    system.energy[1:] += flux_energy * system.surface_area[1:] * dt
 
     system.convert_conserved_to_primitive()
     system.set_boundary_condition()
