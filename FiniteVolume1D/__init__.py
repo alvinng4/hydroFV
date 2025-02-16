2394d2a0386f198a06414dc85466fd9a91a70b33
alvinng4 <alvinng324@gmail.com>
2025-02-16 12:25:40 +0800

    Remove some files

--- FiniteVolume1D/__init__.py
+++ FiniteVolume1D/__init__.py
@@ -1,11 +1,9 @@
 from . import godunov_first_order
-from . import sod_shock
 from . import system
 from . import utils
 
 __all__ = [
     "godunov_first_order",
-    "sod_shock",
     "system",
     "utils",
 ]
