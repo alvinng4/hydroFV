a89efb7eb196cfbb6bdc52ef33cd4df8cf54cf23
alvinng4 <alvinng324@gmail.com>
2025-02-21 16:17:11 +0800

    Fix small bug in sod shock

--- examples/sod_shock.py
+++ examples/sod_shock.py
@@ -135,8 +135,8 @@ def get_initial_system(num_cells: int, coord_sys: str) -> FiniteVolume1D.system.
             system.velocity[i] = U_R
             system.pressure[i] = P_R
 
-    system.convert_primitive_to_conserved()
     system.set_boundary_condition()
+    system.convert_primitive_to_conserved()
 
     return system
 
